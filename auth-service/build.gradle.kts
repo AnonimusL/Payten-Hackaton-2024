8470317df0eede11275c1f8457d8de8ab31c8957
AnonimusL <mijanajdic@gmail.com>
2024-11-30 23:45:14 +0100

    model mapper lib added

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	implementation("org.springframework.security:spring-security-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.modelmapper:modelmapper:3.1.1")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
