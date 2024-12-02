aaf4797bc4d3360543a6e54a43d0b878ea63e8ea
AnonimusL <mijanajdic@gmail.com>
2024-12-03 00:43:22 +0100

    en

--- auth-service/src/main/java/com/payten/hacka/auth_service/runner/AppRunner.java
+++ auth-service/src/main/java/com/payten/hacka/auth_service/runner/AppRunner.java
@@ -3,13 +3,11 @@ package com.payten.hacka.auth_service.runner;
 import com.payten.hacka.auth_service.domain.*;
 import com.payten.hacka.auth_service.repository.*;
 import lombok.AllArgsConstructor;
-import org.hibernate.Cache;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -26,34 +24,44 @@ public class AppRunner {
     @Bean
     CommandLineRunner loadData() {
         return args -> {
-            Address address = new Address("Serbia", "Belgrade",  "11000", "Knez Mihailova", "6/6", 40.5, -10.3, false, null);
+            // Creating and saving an Address
+            Address address = new Address("Serbia", "Serbia", "Belgrade", "11000", "Knez Mihailova", "6/6", 40.5, -10.3, false, null);
             address = addressRepository.save(address);
 
-            Category category = new Category("Education", "Faculty of Computing");
+            // Creating and saving a Category
+            Category category = new Category("Education", "Faculty of Computing", "The best faculty for computer science students");
             category = categoryRepository.save(category);
 
+            // Encoding a password and creating two users
             String ep = passwordEncoder.encode("12345678");
-            User user = new User("email@gmail.rs", ep, "Ime", "Prezime", Role.COMPANY_MANAGER);
-            userRepository.save(user);
+            User user1 = new User("email@gmail.rs", ep, "Ime", "Prezime", Role.COMPANY_MANAGER);
+            user1 = userRepository.save(user1);
 
             User user2 = new User("email2@gmail.rs", ep, "Ime", "Prezime", Role.CLIENT);
+            user2 = userRepository.save(user2);
+
+            // Creating a company and assigning users to it
+            Company company = new Company("RAF", null, address, null, List.of(user1));
+            company = companyRepository.save(company);
+
+            // Linking the users with the company
+            user1.setCompany(company);
+            userRepository.save(user1);
+            user2.setCompany(company);
             userRepository.save(user2);
 
-            Company company = new Company("RAF", address, null, List.of(user));
-            companyRepository.save(company);
+            // Creating two businesses and assigning them to the company and users
+            Business business1 = new Business(category, "RAF", company, List.of(user1));
+            Business business2 = new Business(category, "CET", company, List.of(user1));
 
-            user.setCompany(company);
-            userRepository.save(user);
-
-            Business business1 = new Business(category, "RAF", company, List.of(user));
-            Business business2 = new Business(category, "CET", company, List.of(user));
-            Address address2 = new Address("Serbia", "Belgrade",  "11000", "Knez Mihailova", "6/5", 40.5, -10.3, true, null);
+            Address address2 = new Address("Serbia", "Serbia", "Belgrade", "11000", "Knez Mihailova", "6/5", 40.5, -10.3, true, null);
             address2 = addressRepository.save(address2);
+
             business1.setAddresses(List.of(address2));
 
+            // Saving businesses
             businessRepository.save(business1);
             businessRepository.save(business2);
-
         };
     }
 }
